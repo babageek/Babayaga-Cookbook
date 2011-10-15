e212856215bfee3f69bfe579f5c926946efeabca
Antonio De Marco <antonio@nuvole.org>
2011-10-15 13:18:18 +0200

    working on display suite modes

---
+++ sites/all/themes/babayaga/layouts/left-sidebar.tpl.php
@@ -0,0 +1,13 @@
+<?php print $page_header; ?>
+
+<div id='page'><div id='main-content' class='limiter clearfix'>
+  <?php if ($page['help']) print render($page['help']) ?>
+  <div id='left' class='page-left clearfix position left size-35'><div class='inner'>
+    <?php if (!empty($page['left'])) print render($page['left']) ?>
+  </div></div>    
+  <div id='content' class='page-content clearfix position left size-65'><div class='inner'>
+    <?php if (!empty($page['content'])) print render($page['content']) ?>
+  </div></div>
+</div></div>
+
+<?php print $page_footer; ?>
---
+++ sites/all/themes/babayaga/layouts/right-sidebar.tpl.php
@@ -0,0 +1,13 @@
+<?php print $page_header; ?>
+
+<div id='page'><div id='main-content' class='limiter clearfix inner'>
+  <?php if ($page['help']) print render($page['help']) ?>
+  <div id='content' class='page-content clearfix position left size-65'><div class='inner'>
+    <?php if (!empty($page['content'])) print render($page['content']) ?>
+  </div></div>
+  <div id='right' class='page-left clearfix position left size-35'><div class='inner'>
+    <?php if (!empty($page['right'])) print render($page['right']) ?>
+  </div></div>
+</div></div>
+
+<?php print $page_footer; ?>
