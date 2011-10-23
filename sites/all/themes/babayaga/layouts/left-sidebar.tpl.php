d868f21932ecc1b24144edd8064624633163971e
Antonio De Marco <antonio@nuvole.org>
2011-10-23 16:09:38 +0200

    working on search page

--- sites/all/themes/babayaga/layouts/left-sidebar.tpl.php
+++ sites/all/themes/babayaga/layouts/left-sidebar.tpl.php
@@ -2,10 +2,10 @@
 
 <div id='page'><div id='main-content' class='limiter clearfix'>
   <?php if ($page['help']) print render($page['help']) ?>
-  <div id='left' class='page-left clearfix position left size-35'><div class='inner'>
+  <div id='left' class='page-left clearfix position left size-25'><div class=''>
     <?php if (!empty($page['left'])) print render($page['left']) ?>
   </div></div>    
-  <div id='content' class='page-content clearfix position left size-65'><div class='inner'>
+  <div id='content' class='page-content clearfix position left size-75'><div class='inner'>
     <?php if (!empty($page['content'])) print render($page['content']) ?>
   </div></div>
 </div></div>
